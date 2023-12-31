acb0e00537b1cd6c0f1d2e4e1d41c03eae8b5b3d
Anox071 <153474617+Anox071@users.noreply.github.com>
2023-12-31 18:58:25 +0530

    Update anox30.py

--- anox30.py
+++ anox30.py
@@ -1712,7 +1712,7 @@ def p(uid,pwx,tl):
     'sec-fetch-site': 'same-origin',
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36',',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36',
 }
 			lo = session.post('https://www.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
 			log_cookies=session.cookies.get_dict().keys()
